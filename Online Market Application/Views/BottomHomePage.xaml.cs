6ea28c3c6c054982d7bf7efec3a0c94efd83af45
Chanyx666 <christianresma8@gmail.com>
2021-11-03 13:53:48 +0800

    update

---
+++ Online Market Application/Views/BottomHomePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Online_Market_Application.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BottomHomePage : TabbedPage
+    {
+        public BottomHomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
